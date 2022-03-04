a58b2a1eb8d66fd2cf048541cf2b972315e34bad
ghozzali <100920359+ghozzali@users.noreply.github.com>
2022-03-04 18:05:26 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Helloworld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nama = textBox1.Text;
+            MessageBox.Show("Hai" + nama +" \n Mau yaa Jadi PacarKu Hari ini:*")
+        }
+    }
+}
